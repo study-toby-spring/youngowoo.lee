e1c4dd0cd9ba3a4942cf8182860371fa7860b835
youngwoolee <leeyw910205@gmail.com>
2017-09-02 19:38:48 +0900

    [1.5] Spring의 IoC

--- src/main/java/com/zum/study/factory/DaoFactory.java
+++ src/main/java/com/zum/study/factory/DaoFactory.java
@@ -3,12 +3,16 @@ package com.zum.study.factory;
 import com.zum.study.repository.UserDao;
 import com.zum.study.support.Connector;
 import com.zum.study.support.impl.NaverConnector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Joeylee on 2017-09-02.
  */
+@Configuration
 public class DaoFactory {
 
+    @Bean("userDao")
     public UserDao userDao() {
 
         Connector connector = new NaverConnector();
--- src/main/java/com/zum/study/program/Program.java
+++ src/main/java/com/zum/study/program/Program.java
@@ -5,6 +5,8 @@ import com.zum.study.factory.DaoFactory;
 import com.zum.study.repository.UserDao;
 import com.zum.study.support.Connector;
 import com.zum.study.support.impl.NaverConnector;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -16,21 +18,20 @@ public class Program {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
 
-        DaoFactory daoFactory = new DaoFactory();
-
-        UserDao dao = daoFactory.userDao();
-//        Connector connector = new NaverConnector();
-//        UserDao dao = new UserDao(connector);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = applicationContext.getBean("userDao",UserDao.class);
+//        DaoFactory daoFactory = new DaoFactory();
+//        UserDao dao = daoFactory.userDao();
 
         User user = new User();
 
-        user.setId("hello5");
+        user.setId("hello6");
         user.setName("young woo lee");
         user.setPassword("password");
 
         dao.add(user);
 
-        User found = dao.get("hello5");
+        User found = dao.get("hello6");
 
         System.out.println("name : " + found.getName());
         System.out.println("password : " + found.getPassword());
