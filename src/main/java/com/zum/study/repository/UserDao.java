caab8b28083741a5287c0f2c16bdd34b5b6dba3f
youngwoolee <leeyw910205@gmail.com>
2017-09-06 22:49:40 +0900

    [3.1] 예외 처리 기능을 갖춘 DAO

--- src/main/java/com/zum/study/repository/UserDao.java
+++ src/main/java/com/zum/study/repository/UserDao.java
@@ -23,7 +23,7 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
+    public void add(User user)  throws SQLException {
 
         Connection connection = dataSource.getConnection();
 
@@ -39,7 +39,7 @@ public class UserDao {
         connection.close();
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
+    public User get(String id) throws SQLException {
 
         Connection connection = dataSource.getConnection();
 
@@ -61,6 +61,30 @@ public class UserDao {
         connection.close();
 
         return user;
+    }
+    
+
+    public int getCount() throws SQLException {
+
+        Connection connection = dataSource.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select count(*) from users");
+        ResultSet result = preparedStatement.executeQuery();
+
+        result.next();
+
+        return result.getInt(1);
+    }
+
+    public void deleteAll() throws SQLException {
+
+        Connection connection = dataSource.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("delete * from users");
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
     }
 }
 
