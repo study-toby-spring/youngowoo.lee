d5016d8165d2e4f72de17e900bfc02933bc79a59
youngwoolee <leeyw910205@gmail.com>
2017-10-17 01:40:58 +0900

    각 환경에 맞게 프로퍼티 설정

--- src/main/java/com/zum/study/controller/HomeController.java
+++ src/main/java/com/zum/study/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.zum.study.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Properties;
 
 /**
  * Created by Joeylee on 2017-07-22.
@@ -10,11 +13,23 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
+
+
     @RequestMapping(value = "/hello", method = {RequestMethod.POST, RequestMethod.GET} )
     @ResponseBody
     public String hello(@RequestParam("id") int id) {
         int i= 1;
-        return String.format(id+ "- hello");
+
+        Properties prop = new Properties();
+        try {
+
+        prop.load(HomeController.class.getClassLoader().getResourceAsStream("data.properties"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        String name = prop.getProperty("sample.name");
+        return String.format(id+ "- hello" + name);
     }
 
 }
