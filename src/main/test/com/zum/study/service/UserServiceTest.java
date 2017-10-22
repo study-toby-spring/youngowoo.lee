23f92a3de3c17a24a3d307492e2ca5092f8a21b1
youngwoolee <leeyw910205@gmail.com>
2017-10-22 17:13:02 +0900

    [6.2] 고립된 단위 테스트

--- src/main/test/com/zum/study/service/UserServiceTest.java
+++ src/main/test/com/zum/study/service/UserServiceTest.java
@@ -8,11 +8,16 @@ import com.zum.study.type.Level;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import static org.mockito.Mockito.*;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -46,6 +51,40 @@ public class UserServiceTest {
         users.add(new User("3", "c", "c_pw","woo@gmail.com", Level.SILVER, 60, 29));
         users.add(new User("4", "d", "d_pw","joey@gmail.com", Level.SILVER, 60, 30));
         users.add(new User("5", "e", "e_pw","joeylee@gmail.com", Level.GOLD, 100, 100));
+    }
+
+    @Test
+    public void mockUpgradeLevels() {
+
+        UserDao userDao = mock(UserDao.class);
+        when(userDao.getAll()).thenReturn(users);
+
+        MailSender mailSender = mock(MailSender.class);
+
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.setUserDao(userDao);
+        userService.setMailSender(mailSender);
+
+        userService.upgradeLevels();
+
+        verify(userDao, times(2)).update(any(User.class));
+        verify(userDao, times(2)).update(any(User.class));
+
+        verify(userDao).update(users.get(1));
+        assertThat(users.get(1).getLevel(), is(Level.SILVER));
+
+        verify(userDao).update(users.get(3));
+        assertThat(users.get(3).getLevel(), is(Level.GOLD));
+
+
+        ArgumentCaptor<SimpleMailMessage> message = ArgumentCaptor.forClass(SimpleMailMessage.class);
+
+        verify(mailSender, times(2)).send(message.capture());
+
+        List<SimpleMailMessage> messages = message.getAllValues();
+
+        assertThat(messages.get(0).getTo()[0], is(users.get(1).getEmail()));
+        assertThat(messages.get(1).getTo()[0], is(users.get(3).getEmail()));
     }
 
     @Test
